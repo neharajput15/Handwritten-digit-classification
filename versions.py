42cc44c397f0e701ed392ea1862c491e7f24f2ac
neharajput15 <neharajput0215@gmail.com>
2026-07-16 19:59:29 +0530

    Add files via upload

---
+++ versions.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import numpy as np
+import cv2
+import tensorflow as tf
+import streamlit_drawable_canvas as sdc
+
+print("streamlit==", st.__version__)
+print("numpy==", np.__version__)
+print("opencv-python==", cv2.__version__)
+print("tensorflow==", tf.__version__)
+print("streamlit-drawable-canvas==0.9.3")
